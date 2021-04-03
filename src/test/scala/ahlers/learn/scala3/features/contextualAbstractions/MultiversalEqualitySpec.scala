4c856d549ddf784b49b5e4cf0b90b14cefa1ab06
Michael Ahlers <michael@ahlers.consulting>
2021-04-03 16:47:11 -0700

    Exercise multiversal equality

---
+++ src/test/scala/ahlers/learn/scala3/features/contextualAbstractions/MultiversalEqualitySpec.scala
@@ -0,0 +1,27 @@
+package ahlers.learn.scala3.features.contextualAbstractions
+
+import ahlers.learn.scala3.models.Node
+import ahlers.learn.scala3.models.Node.Tree
+import org.scalatest.wordspec.AnyWordSpec
+import org.scalatest.matchers.should.Matchers.*
+import org.scalatest.wordspec.AnyWordSpec
+import org.scalatestplus.scalacheck.ScalaCheckPropertyChecks
+import ahlers.learn.scala3.models.scalacheck.instances.given
+import org.scalatest.matchers.should.Matchers.*
+import scala.language.strictEquality
+import scala.language.postfixOps
+
+/**
+ * @since April 03, 2021
+ * @author <a href="mailto:michael@ahlers.consulting">Michael Ahlers</a>
+ */
+class MultiversalEqualitySpec extends AnyWordSpec:
+
+  "Nodes" should {
+    "respect multiversal equality" in {
+      def nodeInt:Node[Int] = ???
+      def nodeString:Node[Int] = ???
+      "nodeInt == nodeInt".should(compile)
+      "nodeInt == nodeSting".shouldNot(compile)
+    }
+  }
