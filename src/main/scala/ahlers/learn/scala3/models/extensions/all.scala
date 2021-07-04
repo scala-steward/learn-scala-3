0828437ac60700ca2f7122b5a6206b9aa73e39a0
Scala Steward <me@scala-steward.org>
2021-07-04 20:40:19 +0200

    Reformat with scalafmt 3.0.0-RC6

--- src/main/scala/ahlers/learn/scala3/models/extensions/all.scala
+++ src/main/scala/ahlers/learn/scala3/models/extensions/all.scala
@@ -1,7 +1,9 @@
 package ahlers.learn.scala3.models.extensions
 
 /**
- * @since April 04, 2021
- * @author <a href="mailto:michael@ahlers.consulting">Michael Ahlers</a>
+ * @since April
+ *   04, 2021
+ * @author
+ *   <a href="mailto:michael@ahlers.consulting">Michael Ahlers</a>
  */
 object all extends PersonNameExtension
